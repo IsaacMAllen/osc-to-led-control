9a44d310559deb76ff904885e5481ca2c2daa91d
IsaacMAllen <isaac@isaacallen.dev>
2024-02-19 13:46:46 -0500

    adding liblo

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ RGB_INCDIR=$(RGB_LIB_DISTRIBUTION)/include
 RGB_LIBDIR=$(RGB_LIB_DISTRIBUTION)/lib
 RGB_LIBRARY_NAME=rgbmatrix
 RGB_LIBRARY=$(RGB_LIBDIR)/lib$(RGB_LIBRARY_NAME).a
+
 LDFLAGS+=-L$(RGB_LIBDIR) -l$(RGB_LIBRARY_NAME) -lrt -lm -lpthread
 
 all : lc4
@@ -20,7 +21,7 @@ $(RGB_LIBRARY): FORCE
 lc4.o : lc4.cc
 
 %.o : %.cc
-	$(CXX) -I$(PP_INCDIR) -I$(RGB_INCDIR) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) -I$(RGB_INCDIR) $(CXXFLAGS) -c -o $@ $<
 
 clean:
 	rm -f $(OBJECTS) $(BINARIES)
--- lc4.cc
+++ lc4.cc
@@ -3,25 +3,73 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+
+#include "led-matrix.h"
+
+using rgb_matrix::RGBMatrix;
+using rgb_matrix::Canvas;
 
 #define MIDI_DEVICE "/dev/sequencer"
 
-int main(void) {
-	unsigned char inpacket[4];
+volatile bool interrupt_received = false;
+static void InterruptHandler(int signo) {
+	interrupt_received = true;
+}
 
+static void DrawOnCanvas(RGBMatrix *canvas, uint8_t note, uint8_t velocity) {
+	uint8_t brightness = (float) velocity / 127.0 * 255;
+	canvas->SetBrightness(brightness);
+	switch (note % 3) {
+		case 0:
+			canvas->Fill(0, 0, 255);
+			break;
+		case 1:
+			canvas->Fill(0, 255, 0);
+			break;
+		case 2:
+			canvas->Fill(255, 0, 0);
+			break;
+	}	
+}
+
+int main(int argc, char *argv[]) {
+	RGBMatrix::Options defaults;
+	defaults.hardware_mapping = "adafruit-hat";
+	defaults.rows = 32;
+	defaults.chain_length = 2;
+	defaults.parallel = 1;
+	defaults.show_refresh_rate = false;
+	RGBMatrix *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
+	if (canvas == NULL)
+		return 1;
+
+	signal(SIGTERM, InterruptHandler);
+	signal(SIGINT, InterruptHandler);
+
+	unsigned char inpacket[4];
 	// first open the sequencer device for reading
 	int seqfd = open(MIDI_DEVICE, O_RDONLY);
 	if (seqfd == -1) {
 		printf("Error: cannot open %s\n", MIDI_DEVICE);
+		printf("errno: %d\n", errno);
 		exit(1);
 	}
+	while(!interrupt_received) {
 
-	// now just wait around for MIDI bytes to arrive and print them to screen.
-	while(1) {
+		// now just wait around for MIDI bytes to arrive and print them to screen.
+			
 		read(seqfd, &inpacket, sizeof(inpacket));
 		if (inpacket[0] == MIDI_NOTEON) {
-			printf("Midi note: %d Velocity: %d\n", inpacket[1], inpacket[2]);
+			DrawOnCanvas(canvas, inpacket[1], inpacket[2]);
+			//printf("Midi note: %d Velocity: %d\n", inpacket[1], inpacket[2]);
+			usleep(1*1000);
 		}
 	}
+	
+	canvas->Clear();
+	delete canvas;
+	
 	return 0;
 }
+
